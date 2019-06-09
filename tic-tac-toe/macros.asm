fc824fcd58e16620177dbaebd389283b57c85ad7
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-06-09 10:49:28 +0200

    validate_num macro

---
+++ tic-tac-toe/macros.asm
@@ -0,0 +1,17 @@
+.macro validate_num(%min, %max, %register, %return_addr)
+  .data
+  error_msg: .asciiz "Incorrect number entered"
+
+  .text
+  blt %register, %min, validation_error
+  bgt %register, %max, validation_error
+  j continue
+
+  validation_error:
+    la $a0, error_msg
+    li $v0, 4
+    syscall
+    j %return_addr
+  
+  continue:
+.end_macro
--- tic-tac-toe/tic-tac-toe.asm
+++ tic-tac-toe/tic-tac-toe.asm
@@ -1,26 +1,27 @@
+.include "macros.asm"
 .data
 
-welcome_msg: .ascii "Tic Tac Toe\n"
-choose_rounds_msg: .ascii "Number of rounds (1-5): "
-choose_sign_msg: .ascii "Choose your sign (o or x): "
+welcome_msg: .asciiz "Tic Tac Toe\n"
+choose_rounds_msg: .asciiz "\nNumber of rounds (1-5): "
+choose_sign_msg: .asciiz "Choose your sign (o or x): "
 
 .text
 program_start:
-la $a0, welcome_msg
-li $v0, 4
-syscall
+  la $a0, welcome_msg
+  li $v0, 4
+  syscall
 
-
-la $a0, choose_rounds_msg
-li $v0, 4
-syscall
-
-li $v0, 5
-syscall
-
-
+choose_rounds:
+  la $a0, choose_rounds_msg
+  li $v0, 4
+  syscall
   
-  next_round:
+  li $v0, 5
+  syscall
+  
+  validate_num (1, 5, $v0, choose_rounds)
+
+next_round:
     
   
   
